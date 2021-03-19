41baad7631ad4c1b0b19c4184ba29e14ccbcf379
Daniel-Ignacio-Nieto <ignacionieto65@hotmail.com>
2021-03-19 03:35:30 -0300

    avance de estilos en proyectos

--- client/src/Components/portfolio.js
+++ client/src/Components/portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EP from "../Assets/Imagenes/EcommersePortada.jpg";
 import HM from "../Assets/Imagenes/HenryAppPortada.jpg";
+import "./portfolio.css";
 
 
 
@@ -8,43 +9,67 @@ const Portfolio = () => {
 
 
     return (
-        <section id="Portfolio">
-            <div class="">
-                <h1 class="">Portfolio</h1>
-                <p class="">Aquí te muestro algunos de los proyectos en los cuales trabajé</p>
+        <section id="Portfolio" className="Port-Container">
+            <div className="Port-Intro">
+                <h1>Portfolio</h1>
+                <p>Aquí te muestro algunos de los proyectos en los cuales trabajé</p>
             </div>
-            <div class="">
-                <img src={EP} alt="Foto del Proyecto" class="" />
-                <h2 class="">Patagonia Ecommerse</h2>
-                <p class="">E-commerce de venta de cervezas Patagonia, en el cual trabajamos 6 personas en conjunto aplicando metodoligas Agiles SCRUM</p>
-                <ul>
-                    <li>Javascript</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>Express</li>
-                    <li>NodeJS</li>
-                    <li>Sequelize</li>
-                    <li>PostgreSQL</li>
-                    <li>Passport</li>
-                    <li>Mailgun</li>
-                    <li>CSS Puro</li>
-                </ul>
+            <div className="Port-Proyect">
+                <div className="Port-Proyect-image">
+                    <img src={EP} alt="Foto del Proyecto" />
+                </div>
+                <div className="Port-Proyect-Description">
+                    <div className="Port-P-D-Titles">
+                        <h2>Patagonia Ecommerse</h2>
+                        <p>E-commerce de venta de cervezas Patagonia, en el cual trabajamos 6 personas en conjunto aplicando metodoligas Agiles SCRUM</p>
+                    </div>
+                    <div className="Port-P-D-Tecnologies">
+                        <ul>
+                            <li>Javascript</li>
+                            <li>React</li>
+                            <li>Redux</li>
+                            <li>Express</li>
+                            <li>NodeJS</li>
+                            <li>Sequelize</li>
+                            <li>PostgreSQL</li>
+                            <li>Passport</li>
+                            <li>Mailgun</li>
+                            <li>CSS Puro</li>
+                        </ul>
+                    </div>
+                    <div className="Port-P-D-btns">
+                        <a href="" class="">Sitio Web</a>
+                        <a href="" class="">Repositorio</a>
+                    </div>
+                </div>
             </div>
-            <div class="">
-                <img src={HM} alt="Foto del Proyecto" class="" />
-                <h2 class="">Henry Manager</h2>
-                <p class="">Una Plataforma Web, que facilita la administracion y las estadisticas de desempeño de los estudiantes a lo largo del curso en Henry. </p>
-                <ul>
-                    <li>Typescript</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>MongoDB</li>
-                    <li>Mongoose</li>
-                    <li>NodeJS</li>
-                    <li>Passport</li>
-                    <li>Mailgun</li>
-                    <li>CSS Puro</li>
-                </ul>
+            <div className="Port-Proyect">
+                <div className="Port-Proyect-image">
+                    <img src={HM} alt="Foto del Proyecto" />
+                </div>
+                <div className="Port-Proyect-Description">
+                    <div className="Port-P-D-Titles">
+                        <h2 class="">Henry Manager</h2>
+                        <p class="">Una Plataforma Web, que facilita la administracion y las estadisticas de desempeño de los estudiantes a lo largo del curso en Henry. </p>
+                    </div>
+                    <div class="Port-P-D-Tecnologies">
+                        <ul>
+                            <li>Typescript</li>
+                            <li>React</li>
+                            <li>Redux</li>
+                            <li>MongoDB</li>
+                            <li>Mongoose</li>
+                            <li>NodeJS</li>
+                            <li>Passport</li>
+                            <li>Mailgun</li>
+                            <li>CSS Puro</li>
+                        </ul>
+                    </div>
+                    <div className="Port-P-D-btns">
+                        <a href="" class="">Sitio Web</a>
+                        <a href="" class="">Repositorio</a>
+                    </div>
+                </div>
             </div>
         </section>
     );
