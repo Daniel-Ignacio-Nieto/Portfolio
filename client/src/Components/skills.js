7c05f8bdccd56495a70e7bc95fa8b1d030dde06d
Daniel-Ignacio-Nieto <ignacionieto65@hotmail.com>
2021-06-26 21:41:35 -0300

    skills agregadas

--- client/src/Components/skills.js
+++ client/src/Components/skills.js
@@ -12,75 +12,95 @@ import PSGRE from "../Assets/Iconos/postgresql.png";
 import MONGO from "../Assets/Iconos/MongoDB-hoja.png";
 import PASSP from "../Assets/Iconos/PassportJS.png";
 import MAIL from "../Assets/Iconos/mailgunJS.png";
-import Flip from 'react-reveal/Flip';
+import ANGULAR from "../Assets/Iconos/Angular.png";
+import PWA from "../Assets/Iconos/PWA.png";
+import RXJS from "../Assets/Iconos/Rxjs.png";
+import NGRX from "../Assets/Iconos/NgRx.png";
+import SASS from "../Assets/Iconos/SASS.png";
+import Flip from "react-reveal/Flip";
 import "./skills.css";
 
-
-
 const Skills = () => {
-
-
-    return (
-        <>
-            <div className="Skills-Container">
-                <Flip left>
-                    <div className="Skills-Card">
-                        <img src={JS} alt="JavaScript" class="" height="100" width="100" />
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={TS} alt="Typescript" class="" height="100" width="100" />
-                        <p class="">Typescript</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={HTML} alt="HTML5" class="" height="100" width="100" />
-                        <p class="">HTML5</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={CSS} alt="CSS3" class="" height="100" width="100" />
-                        <p class="">CSS3</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={R3ACT} alt="React" class="" height="100" width="100" />
-                        <p class="">React</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={R3DUX} alt="Redux" class="" height="100" width="100" />
-                        <p class="">Redux</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={NOD} alt="NodeJS" class="" height="100" width="100" />
-                        <p class="">NodeJS</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={EX} alt="Express" class="" height="100" width="100" />
-                        <p class="">Express</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={SQL} alt="Sequelize" class="" height="100" width="100" />
-                        <p class="">Sequelize</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={PSGRE} alt="PostgreSQL" class="" height="100" width="100" />
-                        <p class="">PostgreSQL</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={MONGO} alt="MongoDB" class="" height="100" width="100" />
-                        <p class="">MongoDB</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={PASSP} alt="Passport" class="" height="100" width="100" />
-                        <p class="">Passport</p>
-                    </div>
-                    <div className="Skills-Card">
-                        <img src={MAIL} alt="Mailgun" class="" height="100" width="100" />
-                        <p class="">Mailgun</p>
-                    </div>
-                </Flip>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="Skills-Container">
+				<Flip left>
+					<div className="Skills-Card">
+						<img src={JS} alt="JavaScript" class="" height="100" width="100" />
+						<p>JavaScript</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={TS} alt="Typescript" class="" height="100" width="100" />
+						<p class="">Typescript</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={HTML} alt="HTML5" class="" height="100" width="100" />
+						<p class="">HTML5</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={CSS} alt="CSS3" class="" height="100" width="100" />
+						<p class="">CSS3</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={SASS} alt="CSS3" class="" height="100" width="100" />
+						<p class="">SASS</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={R3ACT} alt="React" class="" height="100" width="100" />
+						<p class="">React</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={ANGULAR} alt="React" class="" height="100" width="100" />
+						<p class="">Angular</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={R3DUX} alt="Redux" class="" height="100" width="100" />
+						<p class="">Redux</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={NGRX} alt="Redux" class="" height="100" width="100" />
+						<p class="">NgRx</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={RXJS} alt="Redux" class="" height="100" width="100" />
+						<p class="">RxJS</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={NOD} alt="NodeJS" class="" height="100" width="100" />
+						<p class="">NodeJS</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={EX} alt="Express" class="" height="100" width="100" />
+						<p class="">Express</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={SQL} alt="Sequelize" class="" height="100" width="100" />
+						<p class="">Sequelize</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={PSGRE} alt="PostgreSQL" class="" height="100" width="100" />
+						<p class="">PostgreSQL</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={MONGO} alt="MongoDB" class="" height="100" width="100" />
+						<p class="">MongoDB</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={PASSP} alt="Passport" class="" height="100" width="100" />
+						<p class="">Passport</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={PWA} alt="Passport" class="" height="100" width="100" />
+						<p class="">PWA</p>
+					</div>
+					<div className="Skills-Card">
+						<img src={MAIL} alt="Mailgun" class="" height="100" width="100" />
+						<p class="">Mailgun</p>
+					</div>
+				</Flip>
+			</div>
+		</>
+	);
 };
-
 
 export default Skills;
